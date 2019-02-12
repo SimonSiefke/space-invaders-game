3f3dc59598376b11ac1bc9b0f4d0b341535c92e9
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-12 11:45:35 +0100

    feat: add shoot

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,45 @@
 <template>
   <section class="container">
     <div class="garden">
-      <div ref="ball" class="ball" />
+      <div
+        ref="ball"
+        class="ball"
+        :style="{ left: `${ball.x}px`, top: `${ball.y}px` }"
+      />
+      <div
+        v-for="bullet in bullets"
+        :key="bullet.id"
+        :style="{
+          left: `${bullet.x}px`,
+          top: `${bullet.y}px`,
+          background: 'purple'
+        }"
+        class="bullet"
+      />
     </div>
     <pre class="output" />
   </section>
 </template>
 
 <script>
+import Vue from 'vue'
 /* eslint-disable no-restricted-globals */
 export default {
+  data() {
+    return {
+      ball: {
+        x: 90,
+        y: 10
+      },
+      bullets: [
+        {
+          x: 10,
+          y: 10,
+          id: 0
+        }
+      ]
+    }
+  },
   mounted() {
     // disallow rotation of screen
     screen.lockOrientationUniversal =
@@ -24,7 +54,7 @@ export default {
     const output = document.querySelector('.output')
     const maxY = garden.clientHeight - ball.clientHeight
 
-    function handleOrientation(event) {
+    const handleOrientation = event => {
       let y = event.gamma // In degree in the range [-90,90]
       output.innerHTML = `gamma: ${y}\n`
 
@@ -32,16 +62,26 @@ export default {
 
       // 10 is half the size of the ball
       // It center the positioning point to the center of the ball
-      // ball.style.top = `${(maxX * x) / 180 - 10}px`
-      ball.style.left = `${(maxY * y) / 180 - 10}px`
+      Vue.set(this.ball, 'x', (maxY * y) / 180 - 10)
     }
 
     window.addEventListener('deviceorientation', handleOrientation)
     window.addEventListener('click', this.shoot)
+    this.loop()
   },
   methods: {
+    loop() {
+      for (const bullet of this.bullets) {
+        bullet.y--
+      }
+      requestAnimationFrame(this.loop)
+    },
     shoot() {
-      //
+      this.bullets.push({
+        x: this.ball.x,
+        y: this.ball.y,
+        id: Math.random()
+      })
     }
   }
 }
@@ -64,12 +104,20 @@ export default {
   border-radius: 10px;
 }
 
+.ball,
+.bullet {
+  position: absolute;
+}
 .ball {
   position: absolute;
   top: 90px;
-  left: 90px;
   width: 20px;
   height: 20px;
   background: green;
+}
+.bullet {
+  width: 3px;
+  height: 3px;
+  background: red;
 }
 </style>
