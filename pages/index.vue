a29f1f891af66a8c7af3317fd597330fc343fa79
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-12 10:03:09 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      hi
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
