6343fbd8450fe16bb2cb5bc6773a2c1ab676fc22
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-12 12:12:48 +0100

    chore: try to enable devtools in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,12 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'universal',
+  generate: {
+    devtools: true
+  },
   server: {
     port: 3000, // default: 3000
-    host: '0.0.0.0', // default: localhost
+    host: '0.0.0.0' // default: localhost
   },
 
   /*
@@ -15,9 +18,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   /*
@@ -54,9 +57,9 @@ module.exports = {
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/,
+          exclude: /(node_modules)/
         })
       }
-    },
-  },
+    }
+  }
 }
